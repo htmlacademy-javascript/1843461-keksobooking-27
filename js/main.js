99fd76d47a2b03404e6e84659b41b63ea43137e0
Albert-Hall <albert.hall.799@gmail.com>
2022-10-12 18:34:06 +0300

    Добавил две функции из задания 2.18 : певрая возвращает случайное целое число, а вторая - случайное число с плавающей запятой, заданного диапазона

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const getRandomInt = (min, max) => {
+    if (min > max) {
+        [min, max] = [max, min];
+    } else if (min === max) {
+        alert('Введите несовпадающие числа');
+        return -1;
+    }
+
+    if (min < 0 || max < 0) {
+        alert('Введите положительные числа');
+        return -1;   
+    }
+
+    return Math.floor(Math.random()*(max - min + 1)) + min
+}
+
+let res = getRandomInt(2, 5);
+// console.log('res1: ', res);
+
+const getRandomNum = (min, max, quantum) => {    
+     if (min > max) {
+        [min, max] = [max, min];
+    } else if (min === max) {
+        alert('Введите несовпадающие числа');
+        return -1;
+    }
+
+    if (min < 0 || max < 0) {
+        alert('Введите положительные числа');
+        return -1;   
+    }
+
+    return +(Math.random()*(max - min) + min).toFixed(quantum)  
+}
+
+res = getRandomNum(1, 3, 4);
+//console.log('res2: ', res)
+
