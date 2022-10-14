8e4d1f98c5bf350ba0d00fec13daf32f48d6803f
Albert-Hall <albert.hall.799@gmail.com>
2022-10-14 14:14:48 +0300

    исправил пробелы и прочую линтоскую  лабуду

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
 const getRandomInt = (min, max) => {
-    if (min > max) {
-        [min, max] = [max, min];
-    } else if (min === max) {
-        alert('Введите несовпадающие числа');
-        return -1;
-    }
+  if (min > max) {
+    [min, max] = [max, min];
+  } else if (min === max) {
+    return -1;
+  }
 
-    if (min < 0 || max < 0) {
-        alert('Введите положительные числа');
-        return -1;   
-    }
+  if (min < 0 || max < 0) {
+    return -1;
+  }
 
-    return Math.floor(Math.random()*(max - min + 1)) + min
-}
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 let res = getRandomInt(2, 5);
 // console.log('res1: ', res);
 
-const getRandomNum = (min, max, quantum) => {    
-     if (min > max) {
-        [min, max] = [max, min];
-    } else if (min === max) {
-        return -1;
-    }
+const getRandomNum = (min, max, quantum) => {
+  if (min > max) {
+    [min, max] = [max, min];
+  } else if (min === max) {
+    return -1;
+  }
 
-    if (min < 0 || max < 0) {
-        return -1;   
-    }
+  if (min < 0 || max < 0) {
+    return -1;
+  }
 
-    return +(Math.random()*(max - min) + min).toFixed(quantum)  
-}
+  return +(Math.random() * (max - min) + min).toFixed(quantum);
+};
 
 res = getRandomNum(1, 3, 4);
 //console.log('res2: ', res)
-
