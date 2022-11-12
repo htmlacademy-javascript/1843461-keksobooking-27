9bf1d078759197ced799ea51caed6d2d9687d66b
Albert-Hall <albert.hall.799@gmail.com>
2022-11-12 12:24:51 +0300

    внес поправку маленькую по удалению нулей после запятой, т.к. в свойстве format.to оно не работало почему-то

--- js/slider.js
+++ js/slider.js
@@ -16,20 +16,21 @@ noUiSlider.create(sliderElem, {
     max: 100000,
   },
   start: 1000,
-  step: 1,
+  step: 100,
   connect: 'lower',
+  behaviour: 'drag',
   format: {
     to: function (value) {
       return value.toFixed(0);
     },
     from: function (value) {
       return parseFloat(value);
-    }
-  }
+    },
+  },
 });
 
-sliderElem.noUiSlider.on('update', () => {
-  pricePerNight.value = sliderElem.noUiSlider.get(true);
+sliderElem.noUiSlider.on('slide', () => {
+  pricePerNight.value = sliderElem.noUiSlider.get(true).toFixed(0); //добавил метод toFixed(0) сюда, т.к. его пропись в свойстве "format" не убирала проблему кучи нулей после запятой в некоторых числах инпута
 });
 
 typeOfLiving.addEventListener('change', (evt) => {
